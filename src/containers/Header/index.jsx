95e689dd9da686fef9f0392908b72eb32f62df36
DmytroB <dmitrijbaraban@gmail.com>
2023-04-07 17:50:57 +0300

    add icons

--- src/containers/Header/index.jsx
+++ src/containers/Header/index.jsx
@@ -5,7 +5,7 @@ import { useLocation } from 'react-router-dom';
 import HeaderTitle from '../../components/HederTitle';
 
 const Header = () => {
-    const { pathname } = useLocation();    
+  const { pathname } = useLocation();
 
   return (
     <div className="header">
@@ -13,17 +13,33 @@ const Header = () => {
         <>
           <HeaderTitle />
           <ul className="list-links">
-            <NavLink to="/users">Users</NavLink>
+            <NavLink to="/users">
+              <img src="assets/users.svg" alt="users_icon" />
+            </NavLink>
             <NavLink to="/profile">
               <img src="assets/user_icon.png" alt="user_icon" />
             </NavLink>
           </ul>
         </>
+      ) : pathname === '/users' ? (
+        <>
+          <HeaderTitle />
+          <NavLink to="/">
+            <img src="assets/home-icon.svg" alt="home_icon" />
+          </NavLink>
+          <NavLink to="/profile">
+            <img src="assets/user_icon.png" alt="user_icon" />
+          </NavLink>
+        </>
       ) : (
         <>
           <HeaderTitle />
-          <NavLink to="/">Feed</NavLink>
-          <NavLink to="/users">Users</NavLink>
+          <NavLink to="/">
+            <img src="assets/home-icon.svg" alt="home_icon" />
+          </NavLink>
+          <NavLink to="/users">
+            <img src="assets/users.svg" alt="users_icon" />
+          </NavLink>
           <NavLink to="/profile">
             <img src="assets/user_icon.png" alt="user_icon" />
           </NavLink>
