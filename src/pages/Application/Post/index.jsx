bfe25715338b4214eadc72910d12175aec5c32db
Dmytro <dmitrijbaraban@gmail.com>
2023-07-27 23:32:37 +0300

    1

--- src/pages/Application/Post/index.jsx
+++ src/pages/Application/Post/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate, NavLink } from 'react-router-dom';
 import api from '../../../services/api';
